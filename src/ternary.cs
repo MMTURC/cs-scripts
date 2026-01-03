afb3c6771a5f8cdb6fd4ba6333467b3bd050b037
mauricio <111543689+MMTURC@users.noreply.github.com>
2026-01-03 14:20:37 -0500

    Showing the ternary operator

---
+++ src/ternary.cs
@@ -0,0 +1,21 @@
+using System.Reflection.Metadata;
+
+static int Max (int a , int b)
+{
+    
+    return (a < b) ? a : b;
+
+
+}
+
+
+// Shorthand for 
+
+if (a < b)
+{
+    return a;
+}
+else
+{
+    return b;
+}
